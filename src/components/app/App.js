561f9cc4a828906e60bd05a080f697d770df4ea6
i-metlyuk <igor.m@core-tech.ru>
2022-01-10 17:32:28 +0300

    finishing lazy import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,13 @@
 import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
     return (
@@ -9,34 +15,36 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Switch>
-                        <Route
-                            exact
-                            path="/"
-                            >
-                            <MainPage></MainPage>
-                        </Route>
-                        
-                        <Route
-                            exact
-                            path="/comics"
-                            >
-                            <ComicsPage></ComicsPage>
-                        </Route>
+                    <Suspense fallback={<Spinner></Spinner>}>
+                        <Switch>
+                            <Route
+                                exact
+                                path="/"
+                                >
+                                <MainPage></MainPage>
+                            </Route>
+                            
+                            <Route
+                                exact
+                                path="/comics"
+                                >
+                                <ComicsPage></ComicsPage>
+                            </Route>
 
-                        <Route
-                            path='/comics/:comicId'
-                            >
-                            <SingleComicPage></SingleComicPage>
-                        </Route>
+                            <Route
+                                path='/comics/:comicId'
+                                >
+                                <SingleComicPage></SingleComicPage>
+                            </Route>
 
-                        <Route
-                            exact
-                            path="*"
-                            >
-                            <Page404></Page404>
-                        </Route>
-                    </Switch>
+                            <Route
+                                exact
+                                path="*"
+                                >
+                                <Page404></Page404>
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </main>
             </div>
         </Router>
